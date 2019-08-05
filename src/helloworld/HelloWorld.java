06ea90658860b60f6d24ca1d02b0c3de2d37600b
bit-user <bit-user@DESKTOP-C1QR4BV>
2019-08-05 19:59:20 +0900

    hi hello

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,8 +3,10 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("asdf");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
